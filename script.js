649e596c8c0ec7fee46cc50a5497b083702b8737
hazeco <thunderwolf2209@gmail.com>
2025-03-12 01:46:35 +0700

    Khet
    test

--- script.js
+++ script.js
@@ -15,3 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+const loader = document.querySelector(".loader");
+const preloader = document.querySelector("#preloader");
+window.addEventListener("load", function(){
+    preloader.style.display = "none"
+    loader.style.display = "none"
+})
