fa29eef9bea1e73633f2663ddefeb057da116cc4
KumPong <ananyodchaichana@gmail.com>
2025-03-11 23:23:45 +0700

    Planai
    Move Eng-game-begin categorise to all game

---
+++ all-game/game-english-beginner/script.js
@@ -0,0 +1,30 @@
+const vocabList = [
+    { word: "apple", meaning: "A kind of fruit"},
+    { word: "banana", meaning: "A long yellow fruit"},
+    { word: "bread", meaning: "Food made from flour"},
+    { word: "milk", meaning: "A white liquid from cows"},
+    { word: "table", meaning: "A flat surface to put things on"},
+    { word: "chair", meaning: "A piece of furniture to sit on"},
+    { word: "bed", meaning: "A place where you sleep"},
+    { word: "father", meaning: "A male parent" },
+    { word: "mother", meaning: "A female parent" },
+    { word: "run", meaning: "Moving fast on foot" },
+    { word: "eat", meaning: "What you do with food" },
+    { word: "drink", meaning: "To take liquid into the mouth" },
+    { word: "sleep", meaning: "To rest with your eyes closed" },
+    { word: "play", meaning: "What children love to do" },
+    { word: "book", meaning: "Something you read" },
+    { word: "pen", meaning: "A writing instrument" },
+    { word: "teacher", meaning: "A person who teaches" },
+    { word: "pocket", meaning: "A small bag in clothes" },
+    { word: "classroom", meaning: "A place where students learn" },
+    { word: "expert", meaning: "A person who knows a lot about something" },
+    { word: "desk", meaning: "A table for studying" },
+    { word: "second", meaning: "One-sixtieth of a minute" },
+    { word: "library", meaning: "A place where you can borrow books" },
+   
+
+
+    
+    
+];
